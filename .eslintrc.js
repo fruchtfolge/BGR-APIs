2a7f7b130cffb0067389ad47f77cd421797e5afe
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-03-10 16:20:03 +0100

    - fix https requests when serving from localhost

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
             "error",
             "unix"
         ],
+        "no-console": 0,
         "quotes": [
             "error",
             "single"
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const utils = require('./src/utils');
-const fs = require('fs')
+const utils = require('./src/utils')
 
 module.exports = {
   soilType(point) {
@@ -13,9 +12,14 @@ module.exports = {
         return resolve(error)
       }
 
-      const url = `https://services.bgr.de/wms/boden/boart1000ob/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=880&WIDTH=514&LAYERS=0&QUERY_LAYERS=0&INFO_FORMAT=text%2Fhtml&I=452&J=371`
+      const options = {
+        hostname: 'services.bgr.de',
+        port: 443,
+        path: `/wms/boden/boart1000ob/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=880&WIDTH=514&LAYERS=0&QUERY_LAYERS=0&INFO_FORMAT=text%2Fhtml&I=452&J=371`,
+        method: 'GET'
+      }
 
-      utils.request(url)
+      utils.request(options)
         .then(res => {
           resolve(utils.soilTypeHtmlParsing(res))
         })
@@ -36,11 +40,15 @@ module.exports = {
         return resolve(error)
       }
 
-      const url = `https://services.bgr.de/wms/boden/bodeneigenschaften/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=880&WIDTH=514&LAYERS=1&QUERY_LAYERS=1&INFO_FORMAT=text%2Fhtml&I=452&J=371`
+      const options = {
+        hostname: 'services.bgr.de',
+        port: 443,
+        path: `/wms/boden/bodeneigenschaften/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=880&WIDTH=514&LAYERS=1&QUERY_LAYERS=1&INFO_FORMAT=text%2Fhtml&I=452&J=371`,
+        method: 'GET'
+      }
 
-      utils.request(url)
+      utils.request(options)
         .then(res => {
-          fs.writeFileSync('test.html', res, 'utf8')
           resolve(utils.soilTypeHtmlParsing(res))
         })
         .catch(error => {
@@ -61,9 +69,14 @@ module.exports = {
         return resolve(error)
       }
 
-      const url = `https://services.bgr.de/wms/boden/sqr1000/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=830&WIDTH=561&LAYERS=${layer}&QUERY_LAYERS=${layer}&INFO_FORMAT=text%2Fhtml`
+      const options = {
+        hostname: 'services.bgr.de',
+        port: 443,
+        path: `/wms/boden/sqr1000/?&REQUEST=GetFeatureInfo&SERVICE=WMS&CRS=EPSG%3A3857&STYLES=default&TRANSPARENT=true&VERSION=1.3.0&FORMAT=image%2Fpng&BBOX=${utils.createBBox(geometry)}&HEIGHT=830&WIDTH=561&LAYERS=${layer}&QUERY_LAYERS=${layer}&INFO_FORMAT=text%2Fhtml`,
+        method: 'GET'
+      }
 
-      utils.request(url)
+      utils.request(options)
         .then(res => {
           resolve(utils.sqrHtmlParsing(res))
         })
